fe6c0bfd139110c5619a65d136573772405e35e0
PabloVegaFundador <204176268+PabloVegaFundador@users.noreply.github.com>
2025-04-04 08:36:23 +0200

    Añade la pantalla de Snackbars con funcionalidad para mostrar mensajes personalizados

--- widgets_app/lib/presentations/screens/snackbar/snackbar_screen.dart
+++ widgets_app/lib/presentations/screens/snackbar/snackbar_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 class SnackbarScreen extends StatelessWidget {
-  
   static const name = 'snackbar_screen';
-  
+
   const SnackbarScreen({super.key});
+
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snackBar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {}),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Snackbars y Diálogos')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
